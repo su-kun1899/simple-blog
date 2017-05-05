5affc9e373b9ccbc629d6570da39e12a9f303e9b
su-kun1899 <higedrum.coz@gmail.com>
2017-05-05 19:26:50 +0900

    Generate blogconfigs controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'blogconfigs/index'
+
+  get 'blogconfigs/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
