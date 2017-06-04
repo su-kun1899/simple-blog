0101befcf48be459f8ba95e17426fc00b92636b3
su-kun1899 <higedrum.coz@gmail.com>
2017-06-04 14:37:01 +0900

    Implements routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,42 @@
 Rails.application.routes.draw do
   get 'blogs/index'
+  get 'blogs', to: 'blogs#index'
+  get 'blogs/:page', to: 'blogs#index'
 
-  get 'blogs/genre'
+  get 'blogs/genre/:id', to: 'blogs#genre'
+  get 'blogs/genre/:id/:page', to: 'blogs#genre'
 
-  get 'blogs/show'
+  get 'blogs/show/:id', to: 'blogs#show'
 
   get 'blogposts/index'
+  get 'blogposts', to: 'blogposts#index'
+
+  get 'blogposts/delete/:id', to: 'blogposts#delete'
+  post 'blogposts/delete', to: 'blogposts#delete'
+  post 'blogposts/delete/:id', to: 'blogposts#delete'
 
   get 'blogposts/add'
+  post 'blogposts/add'
 
-  get 'blogposts/edit'
+  get 'blogposts/:id', to: 'blogposts#edit'
+  patch 'blogposts/:id', to: 'blogposts#edit'
 
   get 'blogposts/delete'
 
   get 'bloggenres/index'
+  get 'bloggenres', to: 'bloggenres#index'
 
   get 'bloggenres/add'
+  post 'bloggenres/add'
 
-  get 'bloggenres/edit'
+  get 'bloggenres/:id', to: 'bloggenres#edit'
+  patch 'bloggenres/:id', to: 'bloggenres#edit'
 
   get 'blogconfigs/index'
+  get 'blogconfigs', to: 'blogconfigs#index'
 
   get 'blogconfigs/edit'
+  patch 'blogconfigs/edit'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
